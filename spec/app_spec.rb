d966f2b25b76e40307b8ce5255510d8726550330
shi1fie <tsutaya42@aisawatatsuya-no-MacBook-Pro.local>
2014-08-23 18:02:03 +0900

    add test

---
+++ spec/app_spec.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require "rack/test"
+require "rspec"
+
+describe "Application" do 
+	include  Rack::Test::Methods
+	def app
+		@app ||= Sinatra::Application
+		
+	end
+
+	describe "/へのアクセス" do
+		before {get "/"}
+
+		it "正常なレスポンスを返すよ！" do
+			expect(last_response).to be_ok
+		end
+
+		it "Helloと出力するよ！" do
+			expect(last_response.body).to eq(
+				"hello, world!...Oops"
+			)
+		end
+	end
+end
